e88b0737c0ffd06baa5cc3625ddb85e92b95ef7d
JPMasterSpeed5 <jp.whiteley@kinkaid.org>
2024-05-10 09:49:27 -0500

    Submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function runProgram(){
-
+    fetchData();
     console.log('runProgram');
     //your code goes here
 }
+
+const apiUrl = 'https://api.brawlapi.com/v1/brawlers';
+
+async function fetchData(){
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    // Do something with the response data
+    console.log(data);
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', runProgram);
